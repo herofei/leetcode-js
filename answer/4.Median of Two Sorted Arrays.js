b4a8f38480a7754062856a7f854532f8f191c787
Mat He <mat.he@qq.com>
2019-02-17 23:18:32 +0800

    [代码提交]

---
+++ answer/4.Median of Two Sorted Arrays.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+
+var findMedianSortedArrays = function(nums1, nums2) {
+    var nums = nums1.concat(nums2),
+        len = nums.length;
+    nums.sort((a,b) => a - b);
+    let mod = len % 2,
+        index;
+    if (mod === 1) {
+        index = Math.floor(len/2);
+        return nums[index];
+    }
+    
+    index = len/2;
+    return (nums[index] + nums[index - 1])/2;
+};
